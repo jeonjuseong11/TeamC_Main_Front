187a94c3ba181233fff9eccdc535a482d5332718
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-05-11 12:04:17 +0900

    Chore : SchoolDetail
    학교 상세 페이지 컴포넌트 폴더 정리
    import 문 절리

--- highway/src/App.js
+++ highway/src/App.js
@@ -1,7 +1,13 @@
 import { ConfigProvider } from "antd";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import TopMenu from "./components/Menu/TopMenu";
+import SchoolDetailInfo from "./components/SchoolDetail/SchoolDetailInfo";
+import SchoolDetailJob from "./components/SchoolDetail/SchoolDetailJob";
+import SchoolDetailReview from "./components/SchoolDetail/SchoolDetailReview";
+import SchoolDetailQuestion from "./components/SchoolDetail/SchoolDetailQuestion";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import OtherSignUp from "./pages/OtherSignUp";
@@ -13,10 +19,6 @@ import StudentSignUp from "./pages/StudentSignUp";
 import UserProfile from "./pages/UserProfile";
 import JoinPresenter from "./pages/JoinPresenter";
 import SchoolDetail from "./pages/SchoolDetail";
-import SchoolDetailInfo from "./components/SchoolDetailInfo";
-import SchoolDetailJob from "./components/SchoolDetailJob";
-import SchoolDetailReview from "./components/SchoolDetailReview";
-import SchoolDetailQuestion from "./components/SchoolDetailQuestion";
 
 function App() {
   return (
@@ -36,7 +38,7 @@ function App() {
             <Route exact path="/schoolranking" element={<SchoolRanking />} />
             <Route exact path="/schooldetail/:schoolId" element={<SchoolDetail />}>
               <Route>
-                <Route exact path="/schooldetail/:schoolId/" element={<SchoolDetailInfo />} />
+                <Route exact path="/schooldetail/:schoolId/info" element={<SchoolDetailInfo />} />
                 <Route exact path="/schooldetail/:schoolId/job" element={<SchoolDetailJob />} />
                 <Route
                   exact
