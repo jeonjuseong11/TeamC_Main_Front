f6c596f6964ae14e955911616827c0a589b11cf1
jeonjuseong11 <jeonjuseong11@gmail.com>
2023-06-09 00:43:57 +0900

    Edit : login
    로그인, 회원가입 input 너비 수정

--- highway/src/components/Slider/AdSlider.js
+++ highway/src/components/Slider/AdSlider.js
@@ -3,10 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./AdSlider.css";
-import styled from "styled-components";
-const SliderWrapper = styled.div`
-  margin: 2px auto;
-`;
 
 function AdSlider() {
   const settings = {
@@ -32,40 +28,15 @@ function AdSlider() {
   };
 
   return (
-    <SliderWrapper>
+    <div>
       <Slider {...settings}>
-        <div>
-          <img
-            style={{ width: "100%", height: "100%", borderRadius: "5px" }}
-            src="https://picsum.photos/1200/320"
-          />
-        </div>
-        <div>
-          <img
-            style={{ width: "100%", height: "100%", borderRadius: "5px" }}
-            src="https://picsum.photos/120/320"
-          />
-        </div>
-        <div>
-          <img
-            style={{ width: "100%", height: "100%", borderRadius: "5px" }}
-            src="https://picsum.photos/100/320"
-          />
-        </div>
-        <div>
-          <img
-            style={{ width: "100%", height: "100%", borderRadius: "5px" }}
-            src="https://picsum.photos/101/320"
-          />
-        </div>
-        <div>
-          <img
-            style={{ width: "100%", height: "100%", borderRadius: "5px" }}
-            src="https://picsum.photos/111/320"
-          />
-        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </Slider>
-    </SliderWrapper>
+    </div>
   );
 }
 
