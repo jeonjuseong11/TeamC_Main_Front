0309e9966e904024a006115e3b52cd7bbe312ec5
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-06-18 00:35:22 +0900

    Feat : HomeBoardList
    홈페이지에 게시판 컴포넌트 추가

--- highway/src/components/SearchForm.js
+++ highway/src/components/SearchForm.js
@@ -1,6 +1,6 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { Button, Col, Input, Row } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { RankTopic } from "./RankSelector";
 
@@ -9,10 +9,13 @@ const SearchInput = styled(Input)`
   height: 3rem;
   border-radius: 50px;
 `;
-const SearchForm = () => {
+const SearchForm = ({ setFilterValue, filterValue }) => {
+  const [searchText, setSearchText] = useState([""]);
+
+  useEffect(() => {}, [searchText, filterValue]);
   return (
     <>
-      <Row justify="center" gutter={[24, 24]} style={{ marginTop: "2rem" }}>
+      <Row justify="center" gutter={[24, 24]} style={{ marginTop: "3rem" }}>
         <Col xs={24} md={15}>
           <h3>나에게 맞는 분야는 무엇일까요?</h3>
         </Col>
@@ -21,6 +24,12 @@ const SearchForm = () => {
         <Col xs={24} md={15}>
           <SearchInput
             placeholder="검색"
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setSearchText([e.target.value]);
+                setFilterValue(searchText);
+              }
+            }}
             prefix={<SearchOutlined style={{ color: "black" }} />}
           />
         </Col>
@@ -30,6 +39,10 @@ const SearchForm = () => {
           {RankTopic.map((item) => {
             return (
               <Button
+                onClick={() => {
+                  setFilterValue(item.value);
+                  setSearchText([""]);
+                }}
                 key={item.value}
                 shape="circle"
                 style={{
