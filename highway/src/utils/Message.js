f7b61d6898d2f1add1542611169cfa9bead9c27c
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-07-06 23:55:55 +0900

    Feat : ProfileRole
    프로필 role(역할) 출력 기능 추가

--- highway/src/utils/Message.js
+++ highway/src/utils/Message.js
@@ -1,4 +1,4 @@
-import { message, Modal } from "antd";
+import { Button, message, Modal } from "antd";
 
 export const info = (props) => {
   message.info(props);
@@ -10,8 +10,35 @@ export const error = (props) => {
     style: { top: "40vh" },
   });
 };
+export const needLoginError = (props, navigate) => {
+  Modal.confirm({
+    title: "로그인이 필요합니다",
+    content: props,
+    centered: true,
+    onOk: () => {
+      navigate("/login");
+    },
+    onCancel: () => {},
+    okText: "로그인창으로",
+    cancelText: "돌아가기",
+  });
+};
 export const success = (props) => {
   Modal.success({
+    content: props,
+  });
+};
+export const needLogin = (props) => {
+  Modal.confirm({
+    content: "로그인이 필요한 서비스입니다.",
+    okText: "이동하기",
+    cancelText: "취소",
+    onOk: props,
+  });
+};
+export const notYourSchool = (props) => {
+  Modal.error({
+    title: "리뷰를 작성 할 수 없습니다.",
     content: props,
     style: { top: "40vh" },
   });
