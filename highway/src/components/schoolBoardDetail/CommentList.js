90682024cc8b800decd460283915be8f2f625172
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-07-14 23:40:37 +0900

    Fixed : CommentList
    댓글 작성자에 따른 수정 삭제 기능 추가

--- highway/src/components/schoolBoardDetail/CommentList.js
+++ highway/src/components/schoolBoardDetail/CommentList.js
@@ -1,6 +1,8 @@
-// CommentList 컴포넌트
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Col, List } from "antd";
+import { Col, Dropdown, List, Menu, Input, Button } from "antd";
+import { CommentDateSpan } from "./SchoolBoardDetailComments";
+import { formatDate } from "./ToggleComment";
+import { EllipsisOutlined, MessageOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import {
   LOAD_POST_COMMENTS_REQUEST,
@@ -9,35 +11,31 @@ import {
   ADD_POST_COMMENT_REQUEST,
 } from "../../constants/actionTypes";
 import { useParams } from "react-router-dom";
-import Comment from "./Comment";
 
 const CommentList = () => {
   const { postId } = useParams();
   const dispatch = useDispatch();
   const { schoolBoardPostComments } = useSelector((state) => state.post);
+  const { me } = useSelector((state) => state.user);
   const [editedComment, setEditedComment] = useState("");
   const [editCommentId, setEditCommentId] = useState(null);
-  const [isReplyVisible, setIsReplyVisible] = useState(false);
-  const [replyTargetId, setReplyTargetId] = useState(null);
   const replyInputRef = useRef(null);
+  const [replyVisibleMap, setReplyVisibleMap] = useState({});
 
   useEffect(() => {
     loadPostComments(postId);
   }, [postId]);
 
-  const loadPostComments = useCallback(
-    (postId) => {
-      if (postId) {
-        dispatch({
-          type: LOAD_POST_COMMENTS_REQUEST,
-          data: {
-            boardId: postId,
-          },
-        });
-      }
-    },
-    [dispatch]
-  );
+  const loadPostComments = (postId) => {
+    if (postId) {
+      dispatch({
+        type: LOAD_POST_COMMENTS_REQUEST,
+        data: {
+          boardId: postId,
+        },
+      });
+    }
+  };
 
   const updatePostComment = useCallback(
     (id, editedComment) => {
@@ -57,6 +55,7 @@ const CommentList = () => {
 
   const removePostComment = useCallback(
     (id) => {
+      console.log("RemovePostComment");
       dispatch({
         type: REMOVE_POST_COMMENT_REQUEST,
         data: {
@@ -68,41 +67,180 @@ const CommentList = () => {
     [dispatch, postId]
   );
 
-  const addPostComment = useCallback(() => {
-    if (editedComment.trim() !== "") {
-      dispatch({
-        type: ADD_POST_COMMENT_REQUEST,
-        data: {
-          content: editedComment,
-          boardId: postId,
-          parentId: replyTargetId,
-        },
-      });
-      setEditedComment("");
-      setReplyTargetId(null);
-      setIsReplyVisible(false);
-    }
-  }, [dispatch, editedComment, postId, replyTargetId]);
+  const addPostComment = useCallback(
+    (parentId) => {
+      if (editedComment.trim() !== "") {
+        dispatch({
+          type: ADD_POST_COMMENT_REQUEST,
+          data: {
+            content: editedComment,
+            boardId: postId,
+            parentId: parentId,
+          },
+        });
+        setEditedComment("");
+        setReplyVisibleMap({});
+      }
+    },
+    [dispatch, editedComment, postId]
+  );
 
-  const handleEdit = useCallback((item) => {
-    setEditedComment(item.content);
-    setEditCommentId(item.id);
-  }, []);
+  const handleEdit = useCallback(
+    (item) => {
+      if (me?.userId === item?.userId) {
+        setEditedComment(item.content);
+        setEditCommentId(item.id);
+      }
+    },
+    [me]
+  );
 
   const cancelEdit = useCallback(() => {
     setEditedComment("");
     setEditCommentId(null);
   }, []);
 
-  const handleReply = useCallback((item) => {
-    setIsReplyVisible(true);
-    setReplyTargetId(item.id);
+  const handleReply = useCallback((id) => {
+    setReplyVisibleMap((prevMap) => ({
+      ...prevMap,
+      [id]: true,
+    }));
   }, []);
 
   const cancelReply = useCallback(() => {
-    setIsReplyVisible(false);
-    setReplyTargetId(null);
+    setReplyVisibleMap({});
   }, []);
+
+  const renderComment = useCallback(
+    (comment) => {
+      const { id, content, userId, children, isDeleted, modifiedDate } = comment;
+      if (isDeleted) {
+        return null;
+      }
+
+      const isEditing = editCommentId === id;
+      const isContentEmpty = editedComment.trim().length === 0;
+      const canEditOrDelete = me?.userId === userId;
+
+      return (
+        <List.Item key={id} style={{ textAlign: "left", marginLeft: "1rem" }}>
+          <List.Item.Meta
+            title={
+              <div>
+                <span>{userId}</span>
+                <span style={{ position: "absolute", right: 0 }}>
+                  {canEditOrDelete && (
+                    <Dropdown
+                      placement="bottomLeft"
+                      overlay={
+                        <Menu>
+                          <Menu.Item onClick={() => handleEdit(comment)}>수정하기</Menu.Item>
+                          <Menu.Item danger onClick={() => removePostComment(id)}>
+                            삭제하기
+                          </Menu.Item>
+                        </Menu>
+                      }
+                      trigger={["hover"]}
+                    >
+                      <EllipsisOutlined />
+                    </Dropdown>
+                  )}
+                </span>
+              </div>
+            }
+            description={
+              isEditing ? (
+                <>
+                  <Input.TextArea
+                    rows={4}
+                    value={editedComment}
+                    style={{ padding: "1rem" }}
+                    onChange={(e) => setEditedComment(e.target.value)}
+                    placeholder="댓글을 입력해주세요"
+                    autoSize={{ minRows: 2, maxRows: 6 }}
+                  />
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      marginTop: "1rem",
+                    }}
+                  >
+                    <Button
+                      type="primary"
+                      onClick={() => updatePostComment(id, editedComment)}
+                      disabled={isContentEmpty || editedComment === content} // 수정된 내용이 없거나 원본과 동일한 경우 버튼 비활성화
+                    >
+                      저장
+                    </Button>
+                    <Button type="text" onClick={cancelEdit}>
+                      취소
+                    </Button>
+                  </div>
+                </>
+              ) : (
+                <div style={{ whiteSpace: "pre-wrap" }}>{content}</div>
+              )
+            }
+          />
+          <CommentDateSpan>{formatDate(modifiedDate)}</CommentDateSpan>
+          <Button type="text" onClick={() => handleReply(id)}>
+            답글달기
+          </Button>
+          {replyVisibleMap[id] && (
+            <div style={{ marginTop: "1rem" }}>
+              <Input.TextArea
+                style={{ padding: "1rem" }}
+                ref={replyInputRef}
+                value={editedComment}
+                onChange={(e) => setEditedComment(e.target.value)}
+                placeholder="댓글을 입력해주세요"
+                autoSize={{ minRows: 2, maxRows: 6 }}
+                autoFocus
+              />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  marginTop: "1rem",
+                }}
+              >
+                <Button type="primary" onClick={() => addPostComment(id)}>
+                  댓글 작성
+                </Button>
+                <Button type="text" onClick={cancelReply}>
+                  취소
+                </Button>
+              </div>
+            </div>
+          )}
+
+          {children?.length > 0 && (
+            <List
+              style={{ marginTop: "1rem" }}
+              size="small"
+              dataSource={children}
+              itemLayout="vertical"
+              renderItem={(reply) => renderComment(reply)}
+            />
+          )}
+        </List.Item>
+      );
+    },
+    [
+      addPostComment,
+      cancelEdit,
+      cancelReply,
+      editCommentId,
+      editedComment,
+      handleEdit,
+      handleReply,
+      me,
+      removePostComment,
+      updatePostComment,
+      replyVisibleMap,
+    ]
+  );
 
   const filteredComments = schoolBoardPostComments.filter(
     (schoolBoardPostComment) => !schoolBoardPostComment.isDeleted
@@ -114,27 +252,7 @@ const CommentList = () => {
         <List
           itemLayout="vertical"
           dataSource={filteredComments}
-          renderItem={(comment) => (
-            <Comment
-              comment={comment}
-              editedComment={editedComment}
-              editCommentId={editCommentId}
-              isReplyVisible={isReplyVisible}
-              replyTargetId={replyTargetId}
-              replyInputRef={replyInputRef}
-              handleEdit={handleEdit}
-              cancelEdit={cancelEdit}
-              removePostComment={removePostComment}
-              handleReply={handleReply}
-              cancelReply={cancelReply}
-              updatePostComment={updatePostComment}
-              addPostComment={addPostComment}
-              setEditedComment={setEditedComment}
-              setEditCommentId={setEditCommentId}
-              setIsReplyVisible={setIsReplyVisible}
-              setReplyTargetId={setReplyTargetId}
-            />
-          )}
+          renderItem={(comment) => renderComment(comment)}
         />
       ) : (
         <></>
