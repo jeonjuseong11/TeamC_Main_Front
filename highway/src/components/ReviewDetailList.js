8564661e7bda47dbc8833055568eb22ff7b56985
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-07-18 16:43:11 +0900

    CSS : SchoolDetail
    학교 상세페이지 간격 수정

--- highway/src/components/ReviewDetailList.js
+++ highway/src/components/ReviewDetailList.js
@@ -1,5 +1,5 @@
-import { StarFilled } from "@ant-design/icons";
-import { Avatar, Button, List, Rate, Skeleton } from "antd";
+import { EllipsisOutlined, StarFilled } from "@ant-design/icons";
+import { Avatar, Button, Dropdown, List, Menu, Rate, Skeleton } from "antd";
 import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,7 +14,10 @@ const DetailReviewsWrapper = styled.div`
   margin-top: 1rem;
   width: 6.5rem;
   float: left;
-  margin-left: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
 `;
 const DetailReviewP = styled.p`
   margin: 0;
@@ -24,14 +27,12 @@ const DetailReviewP = styled.p`
 `;
 const DetailReviewAvatarWrapper = styled.div`
   display: flex;
-  gap: 5rem;
   align-items: center;
   height: 2rem;
-  margin-left: 1rem;
 `;
 const DetailReviewUserTagsWrapper = styled.div`
-  display: flex;
-  margin-left: 9rem;
+  width: 15rem;
+  margin-left: 1.5rem;
 `;
 const DetailReviewContentWrapper = styled.div`
   color: black;
@@ -41,13 +42,12 @@ const DetailReviewContentWrapper = styled.div`
   display: inline-block;
   text-align: left;
   margin-left: 1.5rem;
-  margin-top: 1rem;
+  margin-top: 0.5rem;
 `;
 const ReviewDetailList = ({ setWrite, setEditing, setEditContent }) => {
   const { schoolReviews, loadSchoolReviewsLoading } = useSelector((state) => state.school);
   const { me } = useSelector((state) => state.user);
   const userinfo = JSON.parse(localStorage.getItem("USERINFO"));
-
   const dispatch = useDispatch();
   const { schoolId } = useParams();
 
@@ -72,129 +72,156 @@ const ReviewDetailList = ({ setWrite, setEditing, setEditContent }) => {
 
   return (
     <>
-      <List
-        itemLayout="horizontal"
-        dataSource={filteredReviews}
-        pagination={{
-          align: "center",
-          onChange: (page) => {
-            console.log(page);
-          },
-          pageSize: 2,
-        }}
-        renderItem={(item) => (
-          <List.Item key={item?.id}>
-            <Skeleton loading={loadSchoolReviewsLoading} active>
-              <List.Item.Meta
-                title={
-                  <>
-                    <DetailReviewAvatarWrapper>
-                      <Avatar size={50} style={{ marginTop: "2rem" }}>
-                        {item?.userName && item.userName.length > 0 ? item.userName[0] : ""}
-                      </Avatar>
-                      <h3>{item?.userName}</h3>
-                      <span>
-                        <StarFilled style={{ color: "#FFDC82", marginLeft: "-5rem" }} />
-                        {(item?.trafficRate +
-                          item?.facilityRate +
-                          item?.cafeteriaRate +
-                          item?.educationRate +
-                          item?.employmentRate) /
-                          5}
-                      </span>
-                      {item?.userName === userinfo?.userId && (
-                        <List.Item
-                          action={
-                            <div
-                              style={{
-                                gap: "10px",
-                                fontWeight: "500",
-                                color: "#a2a2a2",
-                              }}
-                            >
-                              <span style={{ cursor: "pointer" }} onClick={() => handleEdit(item)}>
-                                수정
-                              </span>
-                              <br />
-                              <span
-                                style={{ cursor: "pointer" }}
-                                onClick={() => removeReview(item.id)}
-                              >
-                                삭제
-                              </span>
-                            </div>
-                          }
-                        />
-                      )}
-                    </DetailReviewAvatarWrapper>
+      {filteredReviews.length > 0 ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={filteredReviews}
+          pagination={{
+            align: "center",
+            onChange: (page) => {
+              console.log(page);
+            },
+            pageSize: 2,
+          }}
+          renderItem={(item) => (
+            <List.Item key={item?.id}>
+              <Skeleton loading={loadSchoolReviewsLoading} active>
+                <List.Item.Meta
+                  title={
+                    <>
+                      <DetailReviewAvatarWrapper>
+                        <Avatar size={50} style={{ marginTop: "2rem" }}>
+                          {item?.userName && item.userName.length > 0 ? item.userName[0] : ""}
+                        </Avatar>
+                        <div
+                          style={{
+                            width: "65%",
+                            textAlign: "left",
+                            display: "flex",
+                            marginLeft: "5rem",
+                          }}
+                        >
+                          <h3>{item?.userName}</h3>
+                          <p style={{ marginLeft: "1rem", paddingTop: "4px" }}>
+                            <StarFilled style={{ color: "#FFDC82" }} />
+                            {(item?.trafficRate +
+                              item?.facilityRate +
+                              item?.cafeteriaRate +
+                              item?.educationRate +
+                              item?.employmentRate) /
+                              5}
+                          </p>
+                        </div>
 
-                    <DetailReviewUserTagsWrapper>
-                      <TagsItem>{item?.tags}</TagsItem>
-                    </DetailReviewUserTagsWrapper>
-                  </>
-                }
-                description={
-                  <>
-                    <DetailReviewsWrapper>
-                      <DetailReviewP>교통</DetailReviewP>
-                      <Rate
-                        disabled
-                        allowHalf
-                        value={item.trafficRate}
-                        style={{
-                          alignItems: "center",
-                          fontSize: "0.7rem",
-                        }}
-                      />
-                      <DetailReviewP>시설만족도</DetailReviewP>
-                      <Rate
-                        disabled
-                        allowHalf
-                        value={item?.facilityRate}
-                        style={{
-                          alignItems: "center",
-                          fontSize: "0.7rem",
-                        }}
-                      />
-                      <DetailReviewP>급식</DetailReviewP>
-                      <Rate
-                        disabled
-                        allowHalf
-                        value={item?.cafeteriaRate}
-                        style={{
-                          alignItems: "center",
-                          fontSize: "0.7rem",
-                        }}
-                      />
-                      <DetailReviewP>수업만족도</DetailReviewP>
-                      <Rate
-                        disabled
-                        allowHalf
-                        value={item?.educationRate}
-                        style={{
-                          alignItems: "center",
-                          fontSize: "0.7rem",
-                        }}
-                      />
-                      <DetailReviewP>취업</DetailReviewP>
-                      <Rate
-                        disabled
-                        allowHalf
-                        value={item?.employmentRate}
-                        style={{
-                          alignItems: "center",
-                          fontSize: "0.7rem",
-                        }}
-                      />
-                    </DetailReviewsWrapper>
-                    <DetailReviewContentWrapper>{item?.content}</DetailReviewContentWrapper>
-                  </>
-                }
-              />
-            </Skeleton>
-          </List.Item>
-        )}
-      />
+                        {item?.userName == me?.userId && (
+                          <List.Item
+                            style={{ float: "right" }}
+                            actions={[
+                              <Dropdown
+                                placement="bottomLeft"
+                                overlay={
+                                  <Menu>
+                                    <Menu.Item onClick={() => handleEdit(item)}>수정하기</Menu.Item>
+                                    <Menu.Item danger onClick={() => removeReview(item.id)}>
+                                      삭제하기
+                                    </Menu.Item>
+                                  </Menu>
+                                }
+                                trigger={["hover"]}
+                              >
+                                <EllipsisOutlined />
+                              </Dropdown>,
+                            ]}
+                          />
+                        )}
+                      </DetailReviewAvatarWrapper>
+                      <DetailReviewUserTagsWrapper>
+                        <TagsItem style={{ marginLeft: "1rem" }}>{item?.tags}</TagsItem>
+                      </DetailReviewUserTagsWrapper>
+                    </>
+                  }
+                  description={
+                    <>
+                      <DetailReviewsWrapper>
+                        <DetailReviewP>교통</DetailReviewP>
+                        <Rate
+                          disabled
+                          allowHalf
+                          value={item.trafficRate}
+                          style={{
+                            lineHeight: "0.4rem",
+                            alignItems: "center",
+                            fontSize: "0.7rem",
+                            display: "flex",
+                          }}
+                        />
+                        <DetailReviewP>시설만족도</DetailReviewP>
+                        <Rate
+                          disabled
+                          allowHalf
+                          value={item?.facilityRate}
+                          style={{
+                            lineHeight: "0.4rem",
+                            alignItems: "center",
+                            fontSize: "0.7rem",
+                            display: "flex",
+                          }}
+                        />
+                        <DetailReviewP>급식</DetailReviewP>
+                        <Rate
+                          disabled
+                          allowHalf
+                          value={item?.cafeteriaRate}
+                          style={{
+                            alignItems: "center",
+                            lineHeight: "0.4rem",
+                            fontSize: "0.7rem",
+                            display: "flex",
+                          }}
+                        />
+                        <DetailReviewP>수업만족도</DetailReviewP>
+                        <Rate
+                          disabled
+                          allowHalf
+                          value={item?.educationRate}
+                          style={{
+                            alignItems: "center",
+                            lineHeight: "0.4rem",
+                            fontSize: "0.7rem",
+                            display: "flex",
+                          }}
+                        />
+                        <DetailReviewP>취업</DetailReviewP>
+                        <Rate
+                          disabled
+                          allowHalf
+                          value={item?.employmentRate}
+                          style={{
+                            alignItems: "center",
+                            lineHeight: "0.4rem",
+                            fontSize: "0.7rem",
+                            display: "flex",
+                          }}
+                        />
+                      </DetailReviewsWrapper>
+                      <DetailReviewContentWrapper>
+                        {item?.content.split("\n").map((line, index) => (
+                          <React.Fragment key={index}>
+                            {line}
+                            <br />
+                          </React.Fragment>
+                        ))}
+                      </DetailReviewContentWrapper>
+                    </>
+                  }
+                />
+              </Skeleton>
+            </List.Item>
+          )}
+        />
+      ) : (
+        <></>
+      )}
     </>
   );
 };
